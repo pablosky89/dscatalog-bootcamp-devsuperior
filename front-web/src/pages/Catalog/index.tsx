6b4ddbd91b20b1b1ad1f0f7c1dfabd7d33fa252e
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2020-10-23 13:19:45 -0300

    Catalog loaders finished

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react';
 import ProductCard from './components/ProductCard';
 import { Link } from 'react-router-dom';
 import './styles.scss';
-import { makeRequest } from '../../core/utils/request';
-import { ProductsResponse } from '../../core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 
 const Catalog = () => {
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        const params={
+        const params = {
             page: 0,
             linesPerPage: 12
         }
 
-        makeRequest({ url: '/products',params })
-            .then(response => setProductsResponse(response.data));
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
     }, []);
 
 
@@ -25,10 +31,12 @@ const Catalog = () => {
                 Catalogo de productos
     </h1>
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
-                    <Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product} />
-                    </Link>
-                ))}
+                {isLoading ? <ProductCardLoader /> : (
+                    productsResponse?.content.map(product => (
+                        <Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product} />
+                        </Link>
+                    ))
+                )}
             </div>
         </div>
     );
