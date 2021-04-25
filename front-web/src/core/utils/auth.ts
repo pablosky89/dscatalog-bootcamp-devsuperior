58a4161398e04e15ed807e778a0e3d5970075151
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-04-25 18:23:17 -0300

    Add alowed roles on private route

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -1,3 +1,4 @@
+import jwtDecode from 'jwt-decode'
 export const CLIENT_ID = 'dscatalog';
 export const CLIENT_SECRET = 'dscatalog123';
 
@@ -8,6 +9,14 @@ type LoginResponse = {
     scope: string;
     userFirstName: string;
     userId: number;
+}
+
+export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN'
+
+type AccessToken = {
+    exp: number;
+    user_name: string;
+    authorities: Role[];
 }
 
 export const saveSessionData = (loginResponse: LoginResponse) => {
@@ -21,4 +30,34 @@ export const getSessionData = () => {
     const parsedSessionData = JSON.parse(sessionData)
 
     return parsedSessionData as LoginResponse
+}
+
+export const getAccessTokenDecoded = () => {
+    const sessionData = getSessionData();
+
+    const tokenDecoded = jwtDecode(sessionData.access_token)
+
+    return tokenDecoded as AccessToken
+}
+
+export const isTokenValid = () => {
+    const { exp } = getAccessTokenDecoded();
+
+    return Date.now() <= exp * 1000
+}
+
+export const isAuthenticated = () => {
+    const sessionData = getSessionData()
+
+    return sessionData.access_token && isTokenValid()
+}
+
+export const isAllowedByRole = (routeRoles: Role[] = []) => {
+    if (routeRoles.length === 0) {
+        return true
+    }
+
+    const { authorities } = getAccessTokenDecoded();
+
+    return routeRoles.some(role => authorities.includes(role));
 }
