faa10ab0619a9f5660731e04fdee9d237059ca0a
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-04-26 18:34:45 -0300

    finish login/logout layout

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -82,7 +82,7 @@ const Login = () => {
                         </div>
                     )}
                 </div>
-                <Link to="/admin/auth/recover" className="login-link-recover">
+                <Link to="/auth/recover" className="login-link-recover">
                     Esqueci a senha?
                 </Link>
                 <div className="login-submit">
@@ -90,7 +90,7 @@ const Login = () => {
                 </div>
                 <div className="text-center">
                     <span className="not-registed">Não tem Cadastro?</span>
-                    <Link to="/admin/auth/register" className="login-link-register">
+                    <Link to="/auth/register" className="login-link-register">
                         CADASTRAR
                     </Link>
                 </div>
