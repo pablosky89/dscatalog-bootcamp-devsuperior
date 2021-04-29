82c325bf4f25e8204b952c9375660b608b111915
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2021-04-29 16:02:09 -0300

    Finish admin product card layout

--- front-web/src/core/components/ProductPrice/index.tsx
+++ front-web/src/core/components/ProductPrice/index.tsx
@@ -5,17 +5,17 @@ type Props = {
     price: number;
 }
 
-const formatPrice = (price: number)=>{
-    return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2}).format(price);
+const formatPrice = (price: number) => {
+    return new Intl.NumberFormat('pt-BR', { minimumFractionDigits: 2 }).format(price);
 }
 
-const ProductPrice =({ price }: Props) =>(
+const ProductPrice = ({ price }: Props) => (
     <div className="product-price-container">
-    <span className="product-currency">R$</span>
-    <h3 className="product-price">
-        {formatPrice(price)}
-    </h3>
-</div>
+        <span className="product-currency">R$</span>
+        <h3 className="product-price">
+            {formatPrice(price)}
+        </h3>
+    </div>
 
 );
 
